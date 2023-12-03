263672f45f61c978000c191e177d925cf68e9fa6
Alex-Nicalace <anicalace@gmail.com>
2023-12-03 10:34:44 +0200

    renemed Error to Error Message

---
+++ src/ui/ErrorMessage.tsx
@@ -0,0 +1,26 @@
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from 'react-router-dom';
+
+function ErrorMessage() {
+  const navigate = useNavigate();
+  const error = useRouteError();
+  const message = isRouteErrorResponse(error)
+    ? error.statusText
+    : error instanceof Error
+    ? error.message
+    : 'Unknown error';
+  console.log(error);
+
+  return (
+    <div>
+      <h1>Something went wrong 😢</h1>
+      <p>{message}</p>
+      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+    </div>
+  );
+}
+
+export default ErrorMessage;
