06e78a1fa1f038d626460600a9359814ce82e5cf
Alex-Nicalace <anicalace@gmail.com>
2023-12-09 13:02:27 +0200

    flexbox

--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,7 +1,7 @@
 // interface IUsernameProps {}
 
 function Username(): JSX.Element {
-  return <div className="hidden text-sm font-semibold">Alex</div>;
+  return <div className="hidden text-sm font-semibold md:block">Alex</div>;
 }
 
 export default Username;
