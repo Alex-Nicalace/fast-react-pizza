d2e9cd810a83d1bd31b6f9ba418d2f16c13b3da4
Alex-Nicalace <anicalace@gmail.com>
2023-12-10 13:07:46 +0200

    grid

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,25 +1,24 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOverview';
-import Header from './Header';
-import Loader from './Loader';
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
 
-interface IAppLayoutProps {
-  // props
-}
+// interface IAppLayoutProps {}
 
-function AppLayout({}: IAppLayoutProps): JSX.Element {
+function AppLayout(): JSX.Element {
   const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-full grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
 
-      <main>
-        {/* <h1>Content</h1> */}
-        <Outlet />
-      </main>
+      <div className="overflow-y-auto">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
