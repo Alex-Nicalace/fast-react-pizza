d3111d06fc9ada0c087a3d3d5cafb893fc624cf1
Alex-Nicalace <anicalace@gmail.com>
2023-12-11 11:47:04 +0200

    reusing styles with @apply @layer

--- src/features/order/CreateOrder/CreateOrder.tsx
+++ src/features/order/CreateOrder/CreateOrder.tsx
@@ -39,13 +39,13 @@ function CreateOrder(): JSX.Element {
       <Form method="post">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formError?.phone && <p>{formError.phone}</p>}
         </div>
@@ -53,18 +53,7 @@ function CreateOrder(): JSX.Element {
         <div>
           <label>Address</label>
           <div>
-            <input
-              className="
-              w-full rounded-full 
-              border border-stone-200 px-4 
-              py-2 text-sm 
-              transition-all 
-              duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 
-              md:px-6 md:py-3"
-              type="text"
-              name="address"
-              required
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
