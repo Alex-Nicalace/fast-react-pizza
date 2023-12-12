5836b0443075f587f2e17502ee02ab94a4993d87
Alex-Nicalace <anicalace@gmail.com>
2023-12-12 15:38:34 +0200

    styling CreateOrder

--- src/features/order/CreateOrder/action.tsx
+++ src/features/order/CreateOrder/action.tsx
@@ -1,19 +1,19 @@
-import { ActionFunctionArgs, redirect } from 'react-router-dom';
-import { IOrderData, createOrder } from '../../../services/apiRestaurant';
-import { isValidPhone } from '../../../utils/helpers';
+import { ActionFunctionArgs, redirect } from "react-router-dom";
+import { IOrderData, createOrder } from "../../../services/apiRestaurant";
+import { isValidPhone } from "../../../utils/helpers";
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   const order = {
     ...data,
-    cart: JSON.parse(data.cart as string) as IOrderData['cart'],
+    cart: JSON.parse(data.cart as string) as IOrderData["cart"],
     priority: Boolean(data.priority),
   } as IOrderData;
 
   //* <объект с ошибками> потом в компоненте с помощью useActionData передаем этот объект
   const errors: Record<string, string> = {
-    ...(isValidPhone(order.phone) && { phone: 'Invalid phone number' }),
+    ...(!isValidPhone(order.phone) && { phone: "Invalid phone number" }),
   };
   if (Object.keys(errors).length) {
     return errors;
