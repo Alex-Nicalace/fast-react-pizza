ec116236c862719c631237e61195a04f64aac08c
Alex-Nicalace <anicalace@gmail.com>
2023-12-12 12:25:42 +0200

    Configuring Tailwind   Custom Font Family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      // pizza: "Roboto Mono, monospace",
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      // пример дополнения имеющихся стилей своими или переопределения
+      colors: {
+        pizza: "#123456",
+      },
+      fontSize: {
+        huge: ["2.5rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
