12279de050b6b226b1ffd61a50ae4395d25e424b
Alex-Nicalace <anicalace@gmail.com>
2023-12-12 12:55:16 +0200

    styling MenuItem

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,12 +1,12 @@
-import { useLoaderData } from 'react-router-dom';
-import { IPizzaData } from '../../services/apiRestaurant';
-import MenuItem from './MenuItem';
+import { useLoaderData } from "react-router-dom";
+import { IPizzaData } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu(): JSX.Element {
   const menu = useLoaderData() as IPizzaData[];
 
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem key={pizza.id} pizza={pizza} />
       ))}
