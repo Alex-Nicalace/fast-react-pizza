d508f6ea315cd61bde9f9ea16e31b681f2ad25aa
Alex-Nicalace <anicalace@gmail.com>
2023-12-13 11:32:32 +0200

    refactoring main & App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
-// interface IAppProps {}
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import { loader as loaderMenu } from "./features/menu/loader";
+import { loader as loaderOrder } from "./features/order/loader";
+import { action as actionCreateOrder } from "./features/order/CreateOrder/action";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+import ErrorMessage from "./ui/ErrorMessage";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <ErrorMessage />,
+    path: "/",
+    children: [
+      {
+        errorElement: <ErrorMessage />,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+          },
+          {
+            path: "/menu",
+            element: <Menu />,
+            loader: loaderMenu,
+          },
+          {
+            path: "/cart",
+            element: <Cart />,
+          },
+          {
+            path: "/order/new",
+            element: <CreateOrder />,
+            action: actionCreateOrder,
+          },
+          {
+            path: "/order/:orederId",
+            element: <Order />,
+            loader: loaderOrder,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+// interface IAppProps {}
 function App(): JSX.Element {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,59 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import Home from './ui/Home';
-import Menu from './features/menu/Menu';
-import { loader as loaderMenu } from './features/menu/loader';
-import { loader as loaderOrder } from './features/order/loader';
-import { action as actionCreateOrder } from './features/order/CreateOrder/action';
-import Cart from './features/cart/Cart';
-import Order from './features/order/Order';
-import CreateOrder from './features/order/CreateOrder/CreateOrder';
-import AppLayout from './ui/AppLayout';
-import ErrorMessage from './ui/ErrorMessage';
+import App from "./App";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <ErrorMessage />,
-    path: '/',
-    children: [
-      {
-        errorElement: <ErrorMessage />,
-        children: [
-          {
-            index: true,
-            element: <Home />,
-          },
-          {
-            path: '/menu',
-            element: <Menu />,
-            loader: loaderMenu,
-          },
-          {
-            path: '/cart',
-            element: <Cart />,
-          },
-          {
-            path: '/order/new',
-            element: <CreateOrder />,
-            action: actionCreateOrder,
-          },
-          {
-            path: '/order/:orederId',
-            element: <Order />,
-            loader: loaderOrder,
-          },
-        ],
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
