d6d92476af60f38c59e0e0a850a221159d00f6ff
Alex-Nicalace <anicalace@gmail.com>
2023-12-14 12:43:54 +0200

    setting up prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,6 @@
 // prettier.config.js
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
   singleQuote: true,
   jsxSingleQuote: true,
-  semi: false,
-  trailingComma: "all",
 };
