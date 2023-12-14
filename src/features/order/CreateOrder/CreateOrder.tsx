5b67467baed9a4a8cc5891babc72548fc7224ac0
Alex-Nicalace <anicalace@gmail.com>
2023-12-14 13:36:12 +0200

    Using the Cart for New Orders

--- src/features/order/CreateOrder/CreateOrder.tsx
+++ src/features/order/CreateOrder/CreateOrder.tsx
@@ -1,100 +1,87 @@
-import { Form, useActionData, useNavigation } from "react-router-dom";
-import Button from "../../../ui/Button";
-import { useSelector } from "react-redux";
-import { RootStore } from "../../../store";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { Form, useActionData, useNavigation } from 'react-router-dom';
+import Button from '../../../ui/Button';
+import { useSelector } from 'react-redux';
+import { RootStore } from '../../../store';
+import { getCart, getSummary } from '../../cart/cartSlice';
+import EmptyCart from '../../cart/EmptyCart';
+import { formatCurrency } from '../../../utils/helpers';
+import { useState } from 'react';
 
 function CreateOrder(): JSX.Element {
-  // const [withPriority, setWithPriority] = useState(false);
+  const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const { state } = navigation;
-  const isSubmitting = state === "submitting";
+  const isSubmitting = state === 'submitting';
   const formError = useActionData() as Record<string, string>;
   const username = useSelector((state: RootStore) => state.user.userName);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const totalPrice = useSelector(getSummary).totalPrice;
+  const priorityPrice = withPriority ? (totalPrice * 20) / 100 : 0;
+  const totalPriceWithPriority = totalPrice + priorityPrice;
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
-    <div className="px-4 py-6">
-      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+    <div className='px-4 py-6'>
+      <h2 className='mb-8 text-xl font-semibold'>Ready to order? Let's go!</h2>
 
-      <Form method="post">
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label className="sm:basis-40">First Name</label>
+      <Form method='post'>
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>First Name</label>
           <input
-            className="input grow"
-            type="text"
-            name="customer"
+            className='input grow'
+            type='text'
+            name='customer'
             required
             defaultValue={username}
           />
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label className="sm:basis-40">Phone number</label>
-          <div className="grow">
-            <input className="input w-full" type="tel" name="phone" required />
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>Phone number</label>
+          <div className='grow'>
+            <input className='input w-full' type='tel' name='phone' required />
             {formError?.phone && (
-              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+              <p className='mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700'>
                 {formError.phone}
               </p>
             )}
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label className="sm:basis-40">Address</label>
-          <div className="grow">
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>Address</label>
+          <div className='grow'>
             <input
-              className="input w-full"
-              type="text"
-              name="address"
+              className='input w-full'
+              type='text'
+              name='address'
               required
             />
           </div>
         </div>
 
-        <div className="mb-12 flex items-center gap-5">
+        <div className='mb-12 flex items-center gap-5'>
           <input
-            className="h-6 w-6 accent-yellow-400 
-            focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
-            type="checkbox"
-            name="priority"
-            id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            className='h-6 w-6 accent-yellow-400 
+            focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2'
+            type='checkbox'
+            name='priority'
+            id='priority'
+            checked={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label className="font-medium" htmlFor="priority">
+          <label className='font-medium' htmlFor='priority'>
             Want to yo give your order priority?
           </label>
         </div>
 
         <div>
-          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <input type='hidden' name='cart' value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting}>
-            {isSubmitting ? "Sending ..." : "Order now"}
+            {isSubmitting
+              ? 'Sending ...'
+              : `Order now from ${formatCurrency(totalPriceWithPriority)}`}
           </Button>
         </div>
       </Form>
--- src/features/order/CreateOrder/action.tsx
+++ src/features/order/CreateOrder/action.tsx
@@ -1,19 +1,21 @@
-import { ActionFunctionArgs, redirect } from "react-router-dom";
-import { IOrderData, createOrder } from "../../../services/apiRestaurant";
-import { isValidPhone } from "../../../utils/helpers";
+import { ActionFunctionArgs, redirect } from 'react-router-dom';
+import { IOrderData, createOrder } from '../../../services/apiRestaurant';
+import { isValidPhone } from '../../../utils/helpers';
+import store from '../../../store';
+import { claerCart } from '../../cart/cartSlice';
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   const order = {
     ...data,
-    cart: JSON.parse(data.cart as string) as IOrderData["cart"],
+    cart: JSON.parse(data.cart as string) as IOrderData['cart'],
     priority: Boolean(data.priority),
   } as IOrderData;
 
   //* <объект с ошибками> потом в компоненте с помощью useActionData передаем этот объект
   const errors: Record<string, string> = {
-    ...(!isValidPhone(order.phone) && { phone: "Invalid phone number" }),
+    ...(!isValidPhone(order.phone) && { phone: 'Invalid phone number' }),
   };
   if (Object.keys(errors).length) {
     return errors;
@@ -21,6 +23,10 @@ export async function action({ request }: ActionFunctionArgs) {
   //* </ объект с ошибками>
 
   const newOrder = await createOrder(order);
+
+  // * здесь нельзя использовать хуки т.к. это не компонент поэтому надо использовать store
+  // * НО НЕЛЬЗЯ ЭТИМ ЗЛОУПОТРЕБЛЯТЬ т.к. такой способ отключает некоторые оптимизации
+  store.dispatch(claerCart());
 
   return redirect(`/order/${newOrder.id}`);
 }
